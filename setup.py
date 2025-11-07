a74aac22dd78dcece51aceaa44eb4553da5ad382
IanAguiar-ai <iannaianjos@gmail.com>
2025-11-07 15:48:34 -0300

    Version: 0.0.4 - 07-11-2025 - 15:48 - Bug fix try in not import cluster_routes

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=["pandas", "numpy", "numba"],
+    install_requires=["pandas", "numpy", "numba", "scipy"],
     keywords=["python", "regression"],
     classifiers=[
         "Development Status :: 1 - Planning",
--- trips_preprocessing/__init__.py
+++ trips_preprocessing/__init__.py
@@ -1,2 +1,9 @@
+"""
+High-level tools for extracting trip and operation windows from GPS-like
+trajectories, and for clustering routes based on geometric similarity.
+"""
+
 from .extraction import extract_trips, extract_trips_moving_average
 from .route_clustering import cluster_routes
+
+__version__ = "0.0.4"
